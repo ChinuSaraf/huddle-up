bc807ab86fa542a2f59fe924f73c17d93669f81d
ChinuSaraf <csaraf2@illinois.edu>
2023-07-22 17:19:34 -0400

    Notification added

--- src/components/events.jsx
+++ src/components/events.jsx
@@ -5,18 +5,38 @@ import { Button } from "@mui/material";
 
 
 export class Events extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.showNotification = this.showNotification.bind(this);
+    }
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     if (!("Notification" in window)) {
+    //         console.log("Browser does not support desktop notification");
+    //     } else {
+    //         Notification.requestPermission();
+    //     }
+    //   }
+
+    showNotification = async (e) => {
+        e.preventDefault();
+        var options = {
+            body: "This is the body of the Notification",
+            icon: "https://images.pexels.com/photos/853168/pexels-photo-853168.jpeg?    auto=compress&cs=tinysrgb&dpr=1&w=500",
+            dir: "ltr"
+        };
         if (!("Notification" in window)) {
             console.log("Browser does not support desktop notification");
-        } else {
-            Notification.requestPermission();
+        } else if (Notification.permission === "granted") {
+            const notification = new Notification("Notification Demo", options);
+        } else if (Notification.permission === "denied") {
+            await Notification.requestPermission().then((permission) => {
+                if (permission === "granted") {
+                    const notification = new Notification("Notification Demo", options);
+                    // const notification = new Notification('Hey! You got a notification');
+                }
+            });
         }
-      }
-
-    showNotification = (e) => {
-        e.preventDefault();
-        new Notification('Hey! You got a notification');
     }
 
     render() {
